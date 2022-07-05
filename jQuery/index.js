28dc20ec4f5e89a8fdec8831197f377f9c63af92
Vecher13 <ashraf97@yandex.ru>
2022-07-05 23:16:25 +0000

    конец первого дня

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,20 +1,35 @@
 var gamePattern = [];
+var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 function nextSequence () {
     var randomNumber = Math.floor( Math.random() * 4 )
     var randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
-  
-
   }
 
-  function flashAnim(id) { 
+function flashAnim(id) { 
     $("#" + id).fadeOut(100).fadeIn(100)
 }
+function playSound(id) {
+  var audio = new Audio("sounds/" + id + ".mp3")
+  audio.play()
+}
+
+function handler (id){
+  var userChosenColour = id
+  return userChosenColour
+  }
 
 $(".btn").click(function (output) { 
-  flashAnim(output.currentTarget.id)  
+  flashAnim(this.id) 
+  playSound(this.id) 
   nextSequence()
   console.log(gamePattern)
 });
- 
+
+$(".btn").click(function (output) { 
+  userChosenColour = handler(this.id)
+  userClickedPattern.push(userChosenColour)
+console.log(userClickedPattern)
+  
+});
