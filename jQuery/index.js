a1d509fa15bd056386a46dfe53906b0bb5114a3e
Vecher13 <ashraf97@yandex.ru>
2022-07-05 19:07:45 +0000

    Show the Sequence to the User with Animations

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,20 @@
-alert($("h1"))
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+function nextSequence () {
+    var randomNumber = Math.floor( Math.random() * 4 )
+    var randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+  
+
+  }
+
+  function flashAnim(id) { 
+    $("#" + id).fadeOut(100).fadeIn(100)
+}
+
+$(".btn").click(function (output) { 
+  flashAnim(output.currentTarget.id)  
+  nextSequence()
+  console.log(gamePattern)
+});
+ 
